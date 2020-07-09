cf685eb56906d63a7e07e38d6b28c162f8b07db5
long-lehoang <long.bk.khmt@gmail.com>
2020-07-09 10:04:09 +0700

    edit add to cart

--- controllers/buy.php
+++ controllers/buy.php
@@ -2,10 +2,6 @@
 class BuyController extends Controller
 {
 
-    function __construct(){
-        if(!isset($_SESSION['id']))
-            header('Location: '.HOST.'user');
-    }
     public function index()
     {
         $model = $this->model('buy');
